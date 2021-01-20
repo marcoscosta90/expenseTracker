5ca7e7ab1d4e9c3499e63cbda7a9db98947e1d6b
marcoscosta90 <marcoscosta.dev@gmail.com>
2021-01-20 17:07:19 -0300

    Speechly implemented but only in English

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -2,29 +2,29 @@ const incomeColors = ['#123123', '#154731', '#165f40', '#16784f', '#14915f', '#1
 const expenseColors = ['#b50d12', '#bf2f1f', '#c9452c', '#d3583a', '#dc6a48', '#e57c58', '#ee8d68', '#f79d79', '#ffae8a', '#cc474b', '#f55b5f'];
 
 export const incomeCategories = [
-  { type: 'Negocios', amount: 0, color: incomeColors[0] },
-  { type: 'Investimentos', amount: 0, color: incomeColors[1] },
+  { type: 'Business', amount: 0, color: incomeColors[0] },
+  { type: 'Investments', amount: 0, color: incomeColors[1] },
   { type: 'Extras', amount: 0, color: incomeColors[2] },
-  { type: 'Depositos', amount: 0, color: incomeColors[3] },
-  { type: 'Loterias', amount: 0, color: incomeColors[4] },
-  { type: 'Presentes', amount: 0, color: incomeColors[5] },
-  { type: 'Salarios', amount: 0, color: incomeColors[6] },
-  { type: 'Economias', amount: 0, color: incomeColors[7] },
-  { type: 'Alugueis', amount: 0, color: incomeColors[8] },
+  { type: 'Deposits', amount: 0, color: incomeColors[3] },
+  { type: 'Lotery', amount: 0, color: incomeColors[4] },
+  { type: 'Gifts', amount: 0, color: incomeColors[5] },
+  { type: 'Salary', amount: 0, color: incomeColors[6] },
+  { type: 'Savings', amount: 0, color: incomeColors[7] },
+  { type: 'Rent', amount: 0, color: incomeColors[8] },
 ];
 
 export const expenseCategories = [
-  { type: 'Contas', amount: 0, color: expenseColors[0] },
-  { type: 'Carro', amount: 0, color: expenseColors[1] },
-  { type: 'Roupas', amount: 0, color: expenseColors[2] },
-  { type: 'Viagem', amount: 0, color: expenseColors[3] },
-  { type: 'Comida', amount: 0, color: expenseColors[4] },
+  { type: 'Bills', amount: 0, color: expenseColors[0] },
+  { type: 'Car', amount: 0, color: expenseColors[1] },
+  { type: 'Clothes', amount: 0, color: expenseColors[2] },
+  { type: 'Travel', amount: 0, color: expenseColors[3] },
+  { type: 'Food', amount: 0, color: expenseColors[4] },
   { type: 'Shopping', amount: 0, color: expenseColors[5] },
-  { type: 'Casa', amount: 0, color: expenseColors[6] },
-  { type: 'Diversão', amount: 0, color: expenseColors[7] },
-  { type: 'Telefone', amount: 0, color: expenseColors[8] },
-  { type: 'Animais', amount: 0, color: expenseColors[9] },
-  { type: 'Outros', amount: 0, color: expenseColors[10] },
+  { type: 'Home', amount: 0, color: expenseColors[6] },
+  { type: 'Entertainment', amount: 0, color: expenseColors[7] },
+  { type: 'Phone', amount: 0, color: expenseColors[8] },
+  { type: 'Pets', amount: 0, color: expenseColors[9] },
+  { type: 'Others', amount: 0, color: expenseColors[10] },
 ];
 
 export const resetCategories = () => {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider} from '@speechly/react-client'
 
 import  {Provider } from './context/context'
 
@@ -7,7 +8,9 @@ import App from './App';
 import './index.css'
 
 ReactDOM.render(
+    <SpeechProvider appId="0bc63ca0-42ca-44ce-8075-5c77e3eab043" language="en-US">
         <Provider>
     <App />
-        </Provider>, 
+        </Provider>
+    </SpeechProvider>, 
 document.getElementById('root'));
